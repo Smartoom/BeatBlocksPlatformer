848907dae45b2d1e74fe970e8089abd494ef8402
Smartoom <65938705+Smartoom@users.noreply.github.com>
2025-08-09 00:17:49 -0700

    exported sprites. recreated map.changed beat platforms

--- beatblocksplatformer2d/Scripts/color_switch_block.gd
+++ beatblocksplatformer2d/Scripts/color_switch_block.gd
@@ -1,18 +1,12 @@
 extends StaticBody2D
 
-@onready var visual := $ColorRect
+@onready var actived_sprite := $ActivatedSprite
+@onready var deactivated_sprite := $DeactivatedSprite
 @onready var collisionShape := $CollisionShape2D
-
-@export var darkenedColorDarkness :float = 0.5
-var darkColor : Color
-var normalColor :Color
 
 @export var active : bool
 
 func _ready() -> void:
-	normalColor = visual.color
-	darkColor = normalColor.darkened(darkenedColorDarkness)
-	
 	disable()
 	get_tree().root.get_child(0).get_node("BeatEmitter").connect("beat",on_beat_change)
 
@@ -24,11 +18,11 @@ func on_beat_change():
 		disable()
 
 func enable():
-	visual.color =  normalColor
+	actived_sprite.visible = true
+	deactivated_sprite.visible = false
 	collisionShape.disabled = false
-func disable():
-	visual.color = darkColor
-	collisionShape.disabled = true
 
-func _process(delta: float) -> void:
-	pass
+func disable():
+	actived_sprite.visible = false
+	deactivated_sprite.visible = true
+	collisionShape.disabled = true
