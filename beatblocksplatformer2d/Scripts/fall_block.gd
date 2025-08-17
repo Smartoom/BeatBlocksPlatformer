5463a0148855d678fe8d2a436a4ae5b53488ad9f
Smartoom <65938705+Smartoom@users.noreply.github.com>
2025-08-17 15:35:49 -0700

    sand block can be dropped on the level.
    gave background to level 1

--- beatblocksplatformer2d/Scripts/fall_block.gd
+++ beatblocksplatformer2d/Scripts/fall_block.gd
@@ -1,17 +1,19 @@
 extends RigidBody2D
 
 var original_position : Vector2
+var has_fallen = false
 
 func _ready() -> void:
 	original_position = global_position
 
 func _on_timer_timeout() -> void:
 	freeze = false
+	has_fallen = true
 	$ShakeAnimationPlayer.stop()
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if not body.is_in_group("Player"):
+	if not body.is_in_group("Player") or has_fallen:
 		return
 	
 	$ShakeAnimationPlayer.play("Shake")
@@ -20,4 +22,5 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 func reset_object():
 	global_position = original_position
 	linear_velocity = Vector2.ZERO
+	has_fallen = false
 	freeze = true
