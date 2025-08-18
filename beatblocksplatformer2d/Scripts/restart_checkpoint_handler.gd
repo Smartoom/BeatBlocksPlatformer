094ce41f4e8c99849b71ed45e17b1ccf919cc46b
Smartoom <65938705+Smartoom@users.noreply.github.com>
2025-08-17 18:56:07 -0700

    level 3 tiles hell omg
    should add transitions of backgrounds and tiles near the doors door in the end.
    also maybe add a door or a hole that the player comes out of in the beginning.

--- beatblocksplatformer2d/Scripts/restart_checkpoint_handler.gd
+++ beatblocksplatformer2d/Scripts/restart_checkpoint_handler.gd
@@ -1,7 +1,7 @@
 extends Node
 
 @export var checkpoints:Array[Node2D]
-var last_checkpoint :int = 0
+var last_checkpoint :int = -1
 @onready var restart_prompt := $CanvasLayer/RichTextLabel
 #var should_prompt_restart = false
 
@@ -13,7 +13,7 @@ func _process(_delta: float) -> void:
 	if player_node.is_dead:
 		restart_prompt.visible = true
 	
-	if Input.is_action_just_pressed("Restart"):
+	if Input.is_action_just_pressed("Restart") && last_checkpoint!=-1:
 		player_node.position = checkpoints.get(last_checkpoint).global_position
 		restart_prompt.visible = false
 		player_node.revive()
