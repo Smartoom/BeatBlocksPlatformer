6f26947c4c6273ea38a9f868ae62e1cd9f8e76fa
Smartoom <65938705+Smartoom@users.noreply.github.com>
2025-08-17 22:47:17 -0700

    pit, working.
    IT KEEPS BREAKING!!! IT KEPT BREAKING! it is not breaking anymore. i will now push the changes. rejoice!
    i have learned how to use set_deffered and call_deffered functions. i have leveled up! in knowledge. pit is a yes now
    rotator editor attempt.(fail)

---
+++ beatblocksplatformer2d/Scenes/Levels/pit_activator.gd
@@ -0,0 +1,46 @@
+extends Area2D
+
+@export var beat_start :Area2D
+@export var beat_stop :Area2D
+@export var parent_of_appearing_platforms : Node
+var platforms_to_enable := []
+var platforms_enabled = false
+
+func _ready() -> void:
+	for platform in parent_of_appearing_platforms.get_children(false):
+		platforms_to_enable.append(platform)
+	$DisablePlatformsStartTimer.start()
+func _on_disable_platforms_start_timer_timeout() -> void:
+	disable_platforms()
+
+func _on_body_entered_pit(body: Node2D) -> void:
+	if not body.is_in_group("Player") or platforms_enabled:
+		return
+	
+	print("player entered pit")
+	$WaitbeforeEnablePlatformsTimer.start()
+func _on_waitbefore_enable_platforms_timer_timeout() -> void:
+	print("playing is staying in the pit, showing platforms")
+	platforms_enabled = true
+	beat_start.set_deferred("monitoring", true)
+	beat_stop.set_deferred("monitoring", true)
+	
+	enable_platforms()
+
+func enable_platforms():
+	for platform in platforms_to_enable:
+		platform.show_platform()
+
+func disable_platforms():
+	for platform in platforms_to_enable:
+		platform.hide_platform()
+
+func _on_area_2d_2_body_entered(body: Node2D) -> void:
+	if not body.is_in_group("Player") or !platforms_enabled:
+		return
+	print("player left pit")
+	
+	disable_platforms()
+	beat_start.set_deferred("monitoring", false)
+	beat_stop.set_deferred("monitoring", false)
+	platforms_enabled = false
--- beatblocksplatformer2d/Scripts/color_switch_block.gd
+++ beatblocksplatformer2d/Scripts/color_switch_block.gd
@@ -5,12 +5,18 @@ extends StaticBody2D
 @onready var collisionShape := $CollisionShape2D
 
 @export var active : bool
+#var platform_hiddden = false
+var original_position :Vector2
+var hiding_position := Vector2(0,8000)
 
 func _ready() -> void:
 	disable()
+	original_position = position
 	get_tree().root.get_child(0).get_node("BeatEmitter").connect("beat",on_beat_change)
 
 func on_beat_change():
+	#if platform_hiddden:
+		#return
 	active = not active
 	if active:
 		enable()
@@ -18,11 +24,22 @@ func on_beat_change():
 		disable()
 
 func enable():
+	print("AAAAAAAAAAAAAAAAAAAAAAAa+")
 	actived_sprite.visible = true
 	deactivated_sprite.visible = false
 	collisionShape.disabled = false
 
 func disable():
+	print("AAAAAAAAAAAAAAAAAAAAAAAa-")
 	actived_sprite.visible = false
 	deactivated_sprite.visible = true
 	collisionShape.disabled = true
+
+func hide_platform():
+	collisionShape.set_deferred("disabled", true)
+	position = hiding_position
+	visible = false
+func show_platform():
+	collisionShape.set_deferred("disabled", false)
+	position = original_position
+	visible = true
