6a55deed8775a5f72df3f1a4ac85b2c7279ebf7e
Smartoom <65938705+Smartoom@users.noreply.github.com>
2025-08-19 00:00:48 -0700

    AAAAAAAAAAAAAH

---
+++ beatblocksplatformer2d/Scenes/Levels/lava_kill_on_touch.gd
@@ -0,0 +1,28 @@
+extends Area2D
+
+var moving = false
+@export var speed := 20.0
+var original_position:Vector2
+
+func _ready() -> void:
+	original_position = position
+
+func _physics_process(delta: float) -> void:
+	if moving:
+		position += speed * delta * Vector2.UP
+
+func erupt():
+	moving = true
+func stop_erruption():
+	moving = false
+
+func _on_body_entered(body: Node2D) -> void:
+	if not body.is_in_group("Player"):
+		return
+	
+	body.die()
+	stop_erruption()
+
+func reset_object():
+	stop_erruption()
+	position = original_position
--- beatblocksplatformer2d/Scripts/volcano_erruption_trigger.gd
+++ beatblocksplatformer2d/Scripts/volcano_erruption_trigger.gd
@@ -1,18 +1,31 @@
 extends Area2D
 
 @export var player_camera:Camera2D
+@export var original_limit_left = 0
+@export var original_limit_top = 0
+@export var original_limit_right = 0
+@export var original_limit_bottom = 0
+
 @export var new_limit_left = 0
 @export var new_limit_top = 0
 @export var new_limit_right = 0
 @export var new_limit_bottom = 0
-
+@export var lava:Node
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if not body.is_in_group("Player"):
 		return
-	print("new limit")
+	
+	print("erruption trigger")
 	player_camera.limit_left = new_limit_left
 	player_camera.limit_right = new_limit_right
 	player_camera.limit_top = new_limit_top
 	player_camera.limit_bottom = new_limit_bottom
+	lava.erupt()
+
+func reset_object():
+	player_camera.limit_left = original_limit_left
+	player_camera.limit_right = original_limit_right
+	player_camera.limit_top = original_limit_top
+	player_camera.limit_bottom = original_limit_bottom
